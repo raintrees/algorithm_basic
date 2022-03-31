d3345a305be050ceaa410b2b18510d3fd74415d7
lichenlin <17355646536@163.com>
2022-03-31 18:37:10 +0800

    堆 堆排序 加强堆 题目

--- src/class07/Code01_CoverMax.java
+++ src/class07/Code01_CoverMax.java
@@ -73,5 +73,64 @@ public class Code01_CoverMax {
         return ans;
     }
 
+    // 和maxCover2过程是一样的
+    // 只是代码更短
+    // 不使用类定义的写法
+    public static int maxCover3(int[][] m) {
+        // m是二维数组，可以认为m内部是一个一个的一维数组
+        // 每一个一维数组就是一个对象，也就是线段
+        // 如下的code，就是根据每一个线段的开始位置排序
+        // 比如, m = { {5,7}, {1,4}, {2,6} } 跑完如下的code之后变成：{ {1,4}, {2,6}, {5,7} }
+        Arrays.sort(m, (a, b) -> (a[0] - b[0]));
+        // 准备好小根堆
+        PriorityQueue<Integer> heap = new PriorityQueue<>();
+        int max = 0;
+        for (int[] line : m) {
+            while (!heap.isEmpty() && heap.peek() <= line[0]) {
+                heap.poll();
+            }
+            heap.add(line[1]);
+            max = Math.max(max, heap.size());
+        }
+        return max;
+    }
+
+    // for test
+    public static int[][] generateLines(int N, int L, int R) {
+        int size = (int) (Math.random() * N) + 1;
+        int[][] ans = new int[size][2];
+        for (int i = 0; i < size; i++) {
+            int a = L + (int) (Math.random() * (R - L + 1));
+            int b = L + (int) (Math.random() * (R - L + 1));
+            if (a == b) {
+                b = a + 1;
+            }
+            ans[i][0] = Math.min(a, b);
+            ans[i][1] = Math.max(a, b);
+        }
+        return ans;
+    }
+
+    public static void main(String[] args) {
+
+        System.out.println("test begin");
+        int N = 100;
+        int L = 0;
+        int R = 200;
+        int testTimes = 200000;
+        for (int i = 0; i < testTimes; i++) {
+            int[][] lines = generateLines(N, L, R);
+            int ans1 = test(lines);
+            int ans2 = coverMax(lines);
+            int ans3 = maxCover3(lines);
+            if (ans1 != ans2 || ans1 != ans3) {
+                System.out.println("Oops!");
+            }
+        }
+        System.out.println("test end");
+    }
+
+
+
 
 }
